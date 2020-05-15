d60f710289795635ebae44a9a9e5176e410bdf0c
xxgw <540040588@qq.com>
2020-05-16 02:56:37 +0800

    默认按钮和圆角按钮

--- examples/App.vue
+++ examples/App.vue
@@ -1,7 +1,20 @@
 <template>
   <div id="app">
     <!-- <xr-test></xr-test> -->
-    <x-button></x-button>
+    <div class="button-group">
+      <x-button>默认按钮</x-button>
+      <x-button type="primary">主要按钮</x-button>
+      <x-button type="success">成功按钮</x-button>
+      <x-button type="warning">警告按钮</x-button>
+      <x-button type="danger">危险按钮</x-button>
+    </div>
+    <div class="button-group">
+      <x-button round>圆角按钮</x-button>
+      <x-button type="primary" round>主要按钮</x-button>
+      <x-button type="success" round>成功按钮</x-button>
+      <x-button type="warning" round>警告按钮</x-button>
+      <x-button type="danger" round>危险按钮</x-button>
+    </div>
   </div>
 </template>
 
@@ -19,5 +32,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.button-group {
+  width: 100vw;
+  display: flex;
+  justify-content: space-around;
+  margin-top: 50px;
 }
 </style>
--- packages/Button/src/Button.vue
+++ packages/Button/src/Button.vue
@@ -1,12 +1,15 @@
 <template>
   <button
     class="xwy-button"
-    :class="[type?`xwy-button-${type}`:'',{'plain':plain},{'round':round}]"
-  >默认按钮</button>
+    :class="[type?`xwy-button-${type}`:'',{'plain':plain},{'round':round},{'garish':garish}]"
+  >
+    <slot></slot>
+  </button>
 </template>
 
 <script>
 export default {
+  name: 'x-button',
   props: {
     type: {
       type: String,
@@ -25,6 +28,12 @@ export default {
       default() {
         return false
       }
+    },
+    garish: {
+      type: Boolean,
+      default() {
+        return false
+      }
     }
   }
 }
@@ -36,14 +45,95 @@ export default {
   justify-content: center;
   align-items: center;
   vertical-align: middle;
-  padding: 0.5em 1em;
-  border-radius: 5px;
-  border: 1px solid transparent;
+  padding: 0.75em 1.5em;
+  border-radius: 0.3em;
+  border: 1px solid rgba(128, 128, 128, 0.6);
+  user-select: none;
+  color: rgba(0, 0, 0, 0.6);
+  background: white;
   &:focus {
     outline: none;
+    color: $--color-primary-light-2;
+    background: $--color-primary-light-8;
+    border: 1px solid $--color-primary-light-5;
   }
   &:hover {
     cursor: pointer;
+    color: $--color-primary-light-2;
+    background: $--color-primary-light-8;
+    border: 1px solid $--color-primary-light-5;
+  }
+  &:active {
+    border: 1px solid $--color-primary;
+  }
+  &.round {
+    border-radius: 2em;
+  }
+  &.garish {
+  }
+  &.xwy-button-primary {
+    color: rgba(255, 255, 255, 0.9);
+    background: $--color-primary-light-2;
+    border: 1px solid $--color-primary-light-2;
+    &:hover {
+      color: white;
+      background: $--color-primary-light-2;
+    }
+    &:focus {
+      color: white;
+      background: $--color-primary-light-2;
+    }
+    &:active {
+      background: $--color-primary;
+    }
+  }
+  &.xwy-button-success {
+    color: rgba(255, 255, 255, 0.9);
+    background: $--color-success-light-2;
+    border: 1px solid $--color-success-light-2;
+    &:hover {
+      color: white;
+      background: $--color-success-light-2;
+    }
+    &:focus {
+      color: white;
+      background: $--color-success-light-2;
+    }
+    &:active {
+      background: $--color-success;
+    }
+  }
+  &.xwy-button-warning {
+    color: rgba(255, 255, 255, 0.9);
+    background: $--color-warning-light-2;
+    border: 1px solid $--color-warning-light-2;
+    &:hover {
+      color: white;
+      background: $--color-warning-light-2;
+    }
+    &:focus {
+      color: white;
+      background: $--color-warning-light-2;
+    }
+    &:active {
+      background: $--color-warning;
+    }
+  }
+  &.xwy-button-danger {
+    color: rgba(255, 255, 255, 0.9);
+    background: $--color-danger-light-2;
+    border: 1px solid $--color-danger-light-2;
+    &:hover {
+      color: white;
+      background: $--color-danger-light-2;
+    }
+    &:focus {
+      color: white;
+      background: $--color-danger-light-2;
+    }
+    &:active {
+      background: $--color-danger;
+    }
   }
 }
 </style>
