e5751835e508dfd05bf2f4581309ae80bf6c2568
xxgw <540040588@qq.com>
2020-05-16 12:24:41 +0800

    图标按钮和失效按钮

--- examples/App.vue
+++ examples/App.vue
@@ -15,16 +15,37 @@
       <x-button type="warning" round>警告按钮</x-button>
       <x-button type="danger" round>危险按钮</x-button>
     </div>
+    <div class="button-group">
+      <x-button icon="icon-delete">图标按钮</x-button>
+      <x-button type="primary" icon="icon-delete" iconRight>图标靠右</x-button>
+      <x-button type="success" icon="icon-loading" loading>加载按钮</x-button>
+      <x-button type="success" :icon="icon" :loading="loading1" @click.native="toggleLoading()">点击加载</x-button>
+      <x-button type="danger" disabled>失效按钮</x-button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      loading1: false,
+      icon: 'icon-delete'
+    }
+  },
+  methods: {
+    toggleLoading() {
+      this.loading1 ? (this.icon = 'icon-delete') : (this.icon = 'icon-loading')
+      this.loading1 = !this.loading1
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import '//at.alicdn.com/t/font_1500928_oo5tbmvusf.css';
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- packages/Button/src/Button.vue
+++ packages/Button/src/Button.vue
@@ -1,9 +1,13 @@
 <template>
   <button
     class="xwy-button"
-    :class="[type?`xwy-button-${type}`:'',{'plain':plain},{'round':round},{'garish':garish}]"
+    :class="[type?`xwy-button-${type}`:'',{'plain':plain},{'round':round},{'loading':loading},{'disabled':disabled},{'garish':garish},iconRight?'iconRight':'']"
   >
-    <slot></slot>
+    <i v-if="icon" class="iconfont icon" :class="icon"></i>
+    <i v-if="icon && loading" class="iconfont icon" :class="loading?'loading':''"></i>
+    <div class="text">
+      <slot></slot>
+    </div>
   </button>
 </template>
 
@@ -34,33 +38,73 @@ export default {
       default() {
         return false
       }
+    },
+    icon: {
+      type: String,
+      default() {
+        return ''
+      }
+    },
+    iconRight: {
+      type: Boolean,
+      default() {
+        return false
+      }
+    },
+    loading: {
+      type: Boolean,
+      default() {
+        return false
+      }
+    },
+    disabled: {
+      type: Boolean,
+      default() {
+        return false
+      }
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
 .xwy-button {
   display: inline-flex;
   justify-content: center;
   align-items: center;
-  vertical-align: middle;
+  vertical-align: top;
   padding: 0.75em 1.5em;
   border-radius: 0.3em;
   border: 1px solid rgba(128, 128, 128, 0.6);
   user-select: none;
   color: rgba(0, 0, 0, 0.6);
   background: white;
+  > .icon {
+    order: 1;
+    margin-left: 0;
+    margin-right: 0.2em;
+  }
+  > .text {
+    order: 2;
+  }
   &:focus {
     outline: none;
-    color: $--color-primary-light-2;
-    background: $--color-primary-light-8;
+    color: $--color-primary;
+    background: $--color-primary-light-5;
     border: 1px solid $--color-primary-light-5;
   }
   &:hover {
     cursor: pointer;
-    color: $--color-primary-light-2;
-    background: $--color-primary-light-8;
+    color: $--color-primary;
+    background: $--color-primary-light-5;
     border: 1px solid $--color-primary-light-5;
   }
   &:active {
@@ -71,17 +115,32 @@ export default {
   }
   &.garish {
   }
+  &.iconRight {
+    > .icon {
+      order: 2;
+      margin-left: 0.2em;
+      margin-right: 0;
+    }
+    > .text {
+      order: 1;
+    }
+  }
+  &.loading {
+    > .icon {
+      animation: spin 1.5s infinite linear;
+    }
+  }
   &.xwy-button-primary {
     color: rgba(255, 255, 255, 0.9);
     background: $--color-primary-light-2;
     border: 1px solid $--color-primary-light-2;
     &:hover {
       color: white;
-      background: $--color-primary-light-2;
+      background: $--color-primary-light-5;
     }
     &:focus {
       color: white;
-      background: $--color-primary-light-2;
+      background: $--color-primary-light-5;
     }
     &:active {
       background: $--color-primary;
@@ -93,11 +152,11 @@ export default {
     border: 1px solid $--color-success-light-2;
     &:hover {
       color: white;
-      background: $--color-success-light-2;
+      background: $--color-success-light-5;
     }
     &:focus {
       color: white;
-      background: $--color-success-light-2;
+      background: $--color-success-light-5;
     }
     &:active {
       background: $--color-success;
@@ -134,6 +193,10 @@ export default {
     &:active {
       background: $--color-danger;
     }
+  }
+  &.disabled {
+    opacity: 0.6;
+    cursor: not-allowed;
   }
 }
 </style>
